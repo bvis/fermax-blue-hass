71cf7722cfd65caf1290359a2e798ab6a6d02a6c
Basilio Vera <basilio.vera@softonic.com>
2026-04-06 02:07:53 +0200

    feat(event): split into 3 separate entities (doorbell, door_opened, camera_on) for clear history

--- custom_components/fermax_blue/event.py
+++ custom_components/fermax_blue/event.py
@@ -24,6 +24,8 @@ async def async_setup_entry(
 
     for coordinator in coordinators:
         entities.append(FermaxDoorbellEvent(coordinator))
+        entities.append(FermaxDoorOpenedEvent(coordinator))
+        entities.append(FermaxCameraOnEvent(coordinator))
 
     async_add_entities(entities)
 
@@ -32,7 +34,8 @@ class FermaxDoorbellEvent(FermaxBlueEntity, EventEntity):
     """Event entity for doorbell rings."""
 
     _attr_translation_key = "doorbell"
-    _attr_event_types = ["ring", "door_opened", "camera_on"]
+    _attr_event_types = ["ring"]
+    _attr_icon = "mdi:bell-ring"
 
     def __init__(self, coordinator: FermaxBlueCoordinator) -> None:
         super().__init__(coordinator)
@@ -47,38 +50,67 @@ class FermaxDoorbellEvent(FermaxBlueEntity, EventEntity):
                 async_dispatcher_connect(
                     self.hass,
                     SIGNAL_DOORBELL_RING.format(self._device_id, door_name),
-                    self._handle_ring,
+                    self._handle_event,
                 )
             )
+
+    @callback
+    def _handle_event(self) -> None:
+        self._trigger_event("ring")
+        self.async_write_ha_state()
+
+
+class FermaxDoorOpenedEvent(FermaxBlueEntity, EventEntity):
+    """Event entity for door openings."""
+
+    _attr_translation_key = "door_opened"
+    _attr_event_types = ["door_opened"]
+    _attr_icon = "mdi:door-open"
+
+    def __init__(self, coordinator: FermaxBlueCoordinator) -> None:
+        super().__init__(coordinator)
+        self._attr_unique_id = f"{self._device_id}_door_opened_event"
+
+    async def async_added_to_hass(self) -> None:
+        """Register callbacks when added to hass."""
+        await super().async_added_to_hass()
         self.async_on_remove(
             async_dispatcher_connect(
                 self.hass,
                 SIGNAL_DOOR_OPENED.format(self._device_id),
-                self._handle_door_opened,
+                self._handle_event,
             )
         )
+
+    @callback
+    def _handle_event(self) -> None:
+        self._trigger_event("door_opened")
+        self.async_write_ha_state()
+
+
+class FermaxCameraOnEvent(FermaxBlueEntity, EventEntity):
+    """Event entity for camera preview activations."""
+
+    _attr_translation_key = "camera_on"
+    _attr_event_types = ["camera_on"]
+    _attr_icon = "mdi:cctv"
+
+    def __init__(self, coordinator: FermaxBlueCoordinator) -> None:
+        super().__init__(coordinator)
+        self._attr_unique_id = f"{self._device_id}_camera_on_event"
+
+    async def async_added_to_hass(self) -> None:
+        """Register callbacks when added to hass."""
+        await super().async_added_to_hass()
         self.async_on_remove(
             async_dispatcher_connect(
                 self.hass,
                 SIGNAL_CAMERA_ON.format(self._device_id),
-                self._handle_camera_on,
+                self._handle_event,
             )
         )
 
     @callback
-    def _handle_ring(self) -> None:
-        """Handle a doorbell ring event."""
-        self._trigger_event("ring")
-        self.async_write_ha_state()
-
-    @callback
-    def _handle_door_opened(self) -> None:
-        """Handle a door opened event."""
-        self._trigger_event("door_opened")
-        self.async_write_ha_state()
-
-    @callback
-    def _handle_camera_on(self) -> None:
-        """Handle a camera preview started event."""
+    def _handle_event(self) -> None:
         self._trigger_event("camera_on")
         self.async_write_ha_state()
